3eb383da0ca8c64729610f8dce9fab72fb87e806
Dima Kogan <dima@secretsauce.net>
2011-06-05 18:53:54 -0700

    more correct handling of the no-options-given case

--- lib/PDL/Gnuplot.pm
+++ lib/PDL/Gnuplot.pm
@@ -58,11 +58,14 @@ sub new
   {
     my $options = shift;
 
+    # if no options are defined, I'm done
+    my $defaultsOnly;
+    $defaultsOnly = 1 unless keys %$options;
+
     # set some defaults
     $options->{ maxcurves } = 100 unless defined $options->{ maxcurves };
 
-    # if no options are defined, I'm done
-    return '' unless keys %$options;
+    return if $defaultsOnly;
 
 
     # make sure I'm not passed invalid combinations of options
