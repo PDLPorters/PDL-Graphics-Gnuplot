c7609d1d3e8ffc678acbdbf890a9088a8d48afa6
Dima Kogan <dima@secretsauce.net>
2011-08-01 01:03:28 -0700

    minor POD update

--- lib/PDL/Graphics/Gnuplot.pm
+++ lib/PDL/Graphics/Gnuplot.pm
@@ -1644,7 +1644,8 @@ fonts and sizes can be changed.
 
 Everything should work on all platforms that support Gnuplot and Perl. That
 said, C<ONLY> Debian GNU/Linux has been tested to work. Please report successes
-or failures on other platforms to the author.
+or failures on other platforms to the author. A transcript of a failed run with
+{log => 1} would be most helpful.
 
 =head1 REPOSITORY
 
