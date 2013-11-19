87e86ea9e2ddbc141148ed47588124b19541bca5
Craig DeForest <zowie@dhcp-10-72.boulder.swri.edu>
2013-11-19 10:45:53 -0700

    bump version number

--- lib/PDL/Graphics/Gnuplot.pm
+++ lib/PDL/Graphics/Gnuplot.pm
@@ -1771,7 +1771,7 @@ our $MS_io_braindamage = ($^O =~ m/MSWin32/i);    # Do some different things on 
 our $debug_echo = 0;                              # If set, mock up Losedows half-duplex pipes
 
 
-our $VERSION = '2.001';   
+our $VERSION = '2.002';   
 $VERSION = eval $VERSION;
 
 our $gp_version = undef;   # eventually gets the extracted gnuplot(1) version number.
