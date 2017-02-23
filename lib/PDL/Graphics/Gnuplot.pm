39942200702bca3298ce57779fda0a1a32442a0a
Craig DeForest <craig@deforest.org>
2017-02-23 13:45:27 -0700

    increment VERSION; update CHANGES for release

--- lib/PDL/Graphics/Gnuplot.pm
+++ lib/PDL/Graphics/Gnuplot.pm
@@ -1993,7 +1993,7 @@ our $echo_eating = 0;                             # Older versions of gnuplot on
 our $debug_echo = 0;                              # If set, mock up Losedows half-duplex pipes
 
 
-our $VERSION = '2.006_001';
+our $VERSION = '2.007_000';
 $VERSION = eval $VERSION;
 
 our $gp_version = undef;   # eventually gets the extracted gnuplot(1) version number.
