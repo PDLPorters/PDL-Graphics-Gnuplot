9c2b91169a9da8c00e990f9cd34bc4e215079136
Ed J <mohawk2@users.noreply.github.com>
2021-08-11 16:20:28 +0100

    2.018

--- lib/PDL/Graphics/Gnuplot.pm
+++ lib/PDL/Graphics/Gnuplot.pm
@@ -2019,7 +2019,7 @@ our $echo_eating = 0;                             # Older versions of gnuplot on
 our $debug_echo = 0;                              # If set, mock up Losedows half-duplex pipes
 
 
-our $VERSION = '2.017';
+our $VERSION = '2.018';
 $VERSION = eval $VERSION;
 
 our $gp_version = undef;    # eventually gets the extracted gnuplot(1) version number.
